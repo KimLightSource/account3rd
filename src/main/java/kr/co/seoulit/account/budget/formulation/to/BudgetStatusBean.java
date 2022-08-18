f86ad29fb027aa2782f05c44bcbb7651d1dd56f8
KimLightSource <103485784+KimLightSource@users.noreply.github.com>
2022-08-18 09:44:02 +0900

    미세 수정

--- src/main/java/kr/co/seoulit/account/budget/formulation/to/BudgetStatusBean.java
+++ src/main/java/kr/co/seoulit/account/budget/formulation/to/BudgetStatusBean.java
@@ -5,78 +5,87 @@ import kr.co.seoulit.account.sys.base.to.BaseBean;
 import java.util.List;
 
 
-public class BudgetStatusBean extends BaseBean{
-private String accountInnerCode;
-private String accountName;
-private long annualBudgetRecord;//연간 예산 실적
-private long annualBudget;
-private long remainingBudget;
-private double budgetExecRatio;//집행률
-private long monthBudgetRecord;
-private long monthBudget;
-private long remainingMonthBudget;
-private double monthBudgetExecRatio;//집행률
+public class BudgetStatusBean extends BaseBean {
+    private String accountInnerCode;
+    private String accountName;
+    private long ABR;//연간 예산 실적
+    private long annualBudget;
+    private long remainingBudget;
+    private double budgetExecRate;//집행률
+    private long AMBR;
+    private long monthBudget;
+    private long remainingMonthBudget;
+    private double monthBudgetExecRate;//집행률
 
-public String getAccountInnerCode() {
-	return accountInnerCode;
-}
-public void setAccountInnerCode(String accountInnerCode) {
-	this.accountInnerCode = accountInnerCode;
-}
-public String getAccountName() {
-	return accountName;
-}
-public void setAccountName(String accountName) {
-	this.accountName = accountName;
-}
-public long getAnnualBudgetRecord() {
-	return annualBudgetRecord;
-}
-public void setAnnualBudgetRecord(long annualBudgetRecord) {
-	this.annualBudgetRecord = annualBudgetRecord;
-}
-public long getAnnualBudget() {
-	return annualBudget;
-}
-public void setAnnualBudget(long annualBudget) {
-	this.annualBudget = annualBudget;
-}
-public long getRemainingBudget() {
-	return remainingBudget;
-}
-public void setRemainingBudget(long remainingBudget) {
-	this.remainingBudget = remainingBudget;
-}
-public double getBudgetExecRatio() {
-	return budgetExecRatio;
-}
-public void setBudgetExecRatio(double budgetExecRatio) {
-	this.budgetExecRatio = budgetExecRatio;
-}
-public long getMonthBudgetRecord() {
-	return monthBudgetRecord;
-}
-public void setMonthBudgetRecord(long monthBudgetRecord) {
-	this.monthBudgetRecord = monthBudgetRecord;
-}
-public long getMonthBudget() {
-	return monthBudget;
-}
-public void setMonthBudget(long monthBudget) {
-	this.monthBudget = monthBudget;
-}
-public long getRemainingMonthBudget() {
-	return remainingMonthBudget;
-}
-public void setRemainingMonthBudget(long remainingMonthBudget) {
-	this.remainingMonthBudget = remainingMonthBudget;
-}
-public double getMonthBudgetExecRatio() {
-	return monthBudgetExecRatio;
-}
-public void setMonthBudgetExecRatio(double monthBudgetExecRatio) {
-	this.monthBudgetExecRatio = monthBudgetExecRatio;
-}
+    public long getABR() {return ABR;}
+
+    public double getBudgetExecRate() {
+        return budgetExecRate;
+    }
+
+    public void setBudgetExecRate(double budgetExecRate) {
+        this.budgetExecRate = budgetExecRate;
+    }
+
+    public double getMonthBudgetExecRate() {
+        return monthBudgetExecRate;
+    }
+
+    public void setMonthBudgetExecRate(double monthBudgetExecRate) {
+        this.monthBudgetExecRate = monthBudgetExecRate;
+    }
+
+    public void setABR(long ABR) {this.ABR = ABR;}
+    public long getAMBR() {return AMBR;}
+    public void setAMBR(long AMBR) {this.AMBR = AMBR;}
+    public String getAccountInnerCode() {
+        return accountInnerCode;
+    }
+    public void setAccountInnerCode(String accountInnerCode) {
+        this.accountInnerCode = accountInnerCode;
+    }
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public long getAnnualBudget() {
+        return annualBudget;
+    }
+
+    public void setAnnualBudget(long annualBudget) {
+        this.annualBudget = annualBudget;
+    }
+
+    public long getRemainingBudget() {
+        return remainingBudget;
+    }
+
+    public void setRemainingBudget(long remainingBudget) {
+        this.remainingBudget = remainingBudget;
+    }
+
+
+    public long getMonthBudget() {
+        return monthBudget;
+    }
+
+    public void setMonthBudget(long monthBudget) {
+        this.monthBudget = monthBudget;
+    }
+
+    public long getRemainingMonthBudget() {
+        return remainingMonthBudget;
+    }
+
+    public void setRemainingMonthBudget(long remainingMonthBudget) {
+        this.remainingMonthBudget = remainingMonthBudget;
+    }
+
+
 
 
 }
